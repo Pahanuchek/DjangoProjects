935d231c492d988a7f2ef3db9a3ecdc9f8ca2867
Pavel Novozhilov <p.novozhilov24@mail.ru>
2023-05-31 15:44:48 +0300

    Setting up the admin panel

--- People/Humans/admin.py
+++ People/Humans/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Humans
 
-# Register your models here.
+class HumansAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'biography', 'create_at', 'is_published')
+    list_display_links = ['id','name']
+    search_fields = ['name']
+
+
+admin.site.register(Humans, HumansAdmin)
--- People/Humans/models.py
+++ People/Humans/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 class Humans(models.Model):
-    name = models.CharField(max_length=50)
-    biography = models.TextField(blank=None)
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=False)
+    name = models.CharField(max_length=50, verbose_name='Имя')
+    biography = models.TextField(blank=None, verbose_name='Биография')
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания записи')
+    update_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения записи')
+    is_published = models.BooleanField(default=False, verbose_name='Публикация')
+
+    class Meta:
+        verbose_name = 'Человек'
+        verbose_name_plural = 'Люди'
+        ordering = ['-create_at']
