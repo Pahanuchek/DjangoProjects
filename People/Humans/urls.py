eef57892027081ea5c5a4ff8259a7a45a2334793
Pavel Novozhilov <p.novozhilov24@mail.ru>
2023-06-01 16:27:11 +0300

    OK

--- People/Humans/urls.py
+++ People/Humans/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
 from .views import humans
-from django.conf.urls.static import static
-from django.conf import settings
+
 
 urlpatterns = [
     path('', humans)
 ]
 
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
