96d9a66730067f00a737172c3a5f63d5a6f0b77a
Pavel Novozhilov <p.novozhilov24@mail.ru>
2023-06-02 07:18:43 +0300

    Tags

--- People/Humans/views.py
+++ People/Humans/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from .models import Humans
+from .models import Humans, Profession
 
 def humans(request):
     humans = Humans.objects.all()
+    profession = Profession.objects.all()
     context = {
         'humans' : humans,
-        'title' : 'Сайт о людях'
+        'title' : 'Сайт о людях',
+        'profession': profession
     }
     return render(request, 'Humans/humans.html', context=context)
