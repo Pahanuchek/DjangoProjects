7a4e721b3ac52225ee978df3b70ac7509d4f83b7
Pavel Novozhilov <p.novozhilov24@mail.ru>
2023-06-02 09:53:20 +0300

    Create menu

--- People/Humans/urls.py
+++ People/Humans/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import humans
+from .views import humans, get_profession
 
 
 urlpatterns = [
-    path('', humans)
+    path('', humans, name='Home'),
+    path('profession/<int:profession_id>', get_profession, name='Profession')
 ]
 
--- People/Humans/views.py
+++ People/Humans/views.py
@@ -10,3 +10,14 @@ def humans(request):
         'profession': profession
     }
     return render(request, 'Humans/humans.html', context=context)
+
+def get_profession(request, profession_id):
+    humans = Humans.objects.filter(profession_id=profession_id)
+    professions = Profession.objects.all()
+    profession_g = Profession.objects.get(pk=profession_id)
+    context ={
+        'humans' : humans,
+        'professions' : professions,
+        'profession_g' : profession_g
+    }
+    return render(request, 'Humans/profession.html', context=context)
